8d6352defa0a9a8d74fdd99fd1c4106aab36dc7a
chakir elarabi <116119429+codewithchakir@users.noreply.github.com>
2023-08-01 16:22:53 +0100

    portfolio comming soon page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+import fig from './Image16.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={fig} className="personal-fig" alt="fig" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm chakir<br/> and I'm curently building my portfolio
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://m.instagram.com/codewithchakir"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Go back to instagram
         </a>
       </header>
     </div>
